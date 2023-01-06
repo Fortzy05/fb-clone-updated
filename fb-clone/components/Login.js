57983725cdd969c3a25dfefaf75f56880466cd07
fortune <omorodion.fortune@gmail.com>
2023-01-06 08:27:05 -0600

    setting up facebook oauth

--- fb-clone/components/Login.js
+++ fb-clone/components/Login.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 function Login() {
   return (
-    <div>
+    <div className="grid place-items-center">
       <Image
         src="http://links.papareact.com/t4i"
         height={400}
         width={400}
         objectFit="contain"
       />
+      <h1 onClick={signIn} className="p-5 cursor-pointer bg-blue-500 rounded-full text-white text-center">
+        Login with Facebook
+      </h1>
     </div>
   );
 }
--- fb-clone/pages/index.js
+++ fb-clone/pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import Widget from "../components/Widget";
 import { getSession } from "next-auth/react";
+import Login from "../components/Login";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
